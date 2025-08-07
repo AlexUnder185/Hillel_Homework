4c7eb4a9130e53670ae35e3ca5fcc04c82ba65a4
AlexUnder185 <sashap3007@gmail.com>
2025-08-07 14:58:55 +0300

    Update HomeWork3,3.py

--- HomeWork3,3.py
+++ HomeWork3,3.py
@@ -1,11 +1,10 @@
 lst = [2, 4, 7, 11, 0, -2, 8]
+
 length = len(lst)
 middle = (length + 1) // 2
 
 lst1 = lst[:middle]
 lst2 = lst[middle:]
-if lst == []:
-    print([[], []])
-else:
-    print(lst1)
-    print(lst2)
+lst3 = [lst1,lst2]
+
+print(lst3)
