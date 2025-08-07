5b46f94e21427d971ff6e65baa7adcc01d9d237b
AlexUnder185 <sashap3007@gmail.com>
2025-08-07 11:15:44 +0300

    Update HomeWork3,3.py

--- HomeWork3,3.py
+++ HomeWork3,3.py
@@ -1,6 +1,11 @@
 lst = [2, 4, 7, 11, 0, -2, 8]
-half = len(lst) // 2
-lst1 = lst[:half]
-lst2 = lst[half:]
-print(lst1)
-print(lst2)
+length = len(lst)
+middle = (length + 1) // 2
+
+lst1 = lst[:middle]
+lst2 = lst[middle:]
+if length == 0:
+    print([[], []])
+else:
+    print(lst1)
+    print(lst2)
